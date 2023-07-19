fc39ab398a0bd4f714c6551b97c86af80d3e8ca8
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-07-19 12:24:58 +0200

    new data for database

--- backend/database.sql
+++ backend/database.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS `plant` (
   `family_plant` VARCHAR(100) NOT NULL,
   `description` VARCHAR(550) NULL,
   `add_date` DATETIME DEFAULT NOW(),
+  `plant_water` DATETIME DEFAULT NOW(),
   `user_id` INT,  
   FOREIGN KEY (user_id) REFERENCES user(id)
 );
@@ -34,8 +35,9 @@ VALUES
 
 -- Insertion des données dans la table "plant"
 
-INSERT INTO plant (firstname_plant, family_plant, description, add_date, user_id)
+INSERT INTO plant (firstname_plant, family_plant, description, add_date, plant_water, user_id)
 VALUES
-("Nyx", "Spathiphyllum (Fleur de lune)", "Ce sont souvent des fleurs dites sensuelles, au parfum naturellement entêtant, enveloppant et charnel. Elles sont souvent dites miellées, épicées ou fruitées.", NOW(), 1),
-("Andréa", "Aspidistra", "L'Aspidistra est une plante vivace rhizomateuse à feuillage vert foncé, persistant luisant, dressé et lancéolé. Les feuilles atteignent jusqu'à 60 cm hauteur tandis que la plante s'étale sur environ 50 cm de largeur. Les fleurs minuscules, de couleur pourpre, éclosent au ras du sol.", NOW(), 1),
-("Bob", "Aspidistra", "L'Aspidistra est une plante vivace rhizomateuse à feuillage vert foncé, persistant luisant, dressé et lancéolé. Les feuilles atteignent jusqu'à 60 cm hauteur tandis que la plante s'étale sur environ 50 cm de largeur. Les fleurs minuscules, de couleur pourpre, éclosent au ras du sol.", NOW(), 2);
+("Nyx", "Spathiphyllum (Fleur de lune)", "Ce sont souvent des fleurs dites sensuelles, au parfum naturellement entêtant, enveloppant et charnel. Elles sont souvent dites miellées, épicées ou fruitées.", NOW(), NOW(), 1),
+("Andréa", "Aspidistra", "L'Aspidistra est une plante vivace rhizomateuse à feuillage vert foncé, persistant luisant, dressé et lancéolé. Les feuilles atteignent jusqu'à 60 cm hauteur tandis que la plante s'étale sur environ 50 cm de largeur. Les fleurs minuscules, de couleur pourpre, éclosent au ras du sol.", NOW(), NOW(), 1),
+("Bob", "Aspidistra", "L'Aspidistra est une plante vivace rhizomateuse à feuillage vert foncé, persistant luisant, dressé et lancéolé. Les feuilles atteignent jusqu'à 60 cm hauteur tandis que la plante s'étale sur environ 50 cm de largeur. Les fleurs minuscules, de couleur pourpre, éclosent au ras du sol.", NOW(), NOW(), 2);
+
