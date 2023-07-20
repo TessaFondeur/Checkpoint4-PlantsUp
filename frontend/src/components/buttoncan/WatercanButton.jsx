bc6fe3d7fe3462a6a7f8d08386e517c5778dd76c
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-07-20 15:58:13 +0200

    New component FRONT for the watering can with animation.

---
+++ frontend/src/components/buttoncan/WatercanButton.jsx
@@ -0,0 +1,11 @@
+import "./WatercanButton.scss";
+
+export default function WatercanButton() {
+  return (
+    <div className="container-watercan">
+      <button className="btn-water-can" type="button">
+        <img src="./src/assets/arrosoir1.png" alt="Arrosoir" />
+      </button>
+    </div>
+  );
+}
